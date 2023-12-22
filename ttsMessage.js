3f19f9c8958e63d7144294585a39d800b56a64d0
Yashs Tiwari <yashstiwari03@gmail.com>
2023-12-22 14:17:23 +0530

    tts

--- ttsMessage.js
+++ ttsMessage.js
@@ -1,7 +1,8 @@
 const textToSpeech = require("@google-cloud/text-to-speech");
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
-async function textToSpeechAndPlay(text) {
+const textToSpeechAndPlay = async (req, res) => {
+  const text = req.body.text;
   if (!text) {
     console.error("No text provided");
     return;
@@ -11,7 +12,7 @@ async function textToSpeechAndPlay(text) {
 
   const request = {
     input: { text: text },
-    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    voice: { languageCode: "ta-IN", ssmlGender: "NEUTRAL" },
     audioConfig: { audioEncoding: "MP3" },
   };
 
@@ -20,7 +21,9 @@ async function textToSpeechAndPlay(text) {
     const audioContent = response.audioContent;
 
     exec(
-     ` echo "${audioContent.toString("base64")}" | base64 --decode | play -t mp3 -`,
+      ` echo "${audioContent.toString(
+        "base64"
+      )}" | base64 --decode | play -t mp3 -`,
       (error) => {
         if (error) {
           console.error("Error playing audio:", error);
@@ -31,8 +34,13 @@ async function textToSpeechAndPlay(text) {
     );
   } catch (err) {
     console.error("Error occurred:", err);
+  } finally {
+    res.status(200).json({ message: "Text to Speech and audio played" });
+    ttsClient.close();
   }
-}
+};
 
 // Example usage
-textToSpeechAndPlay("Hello, this is a test of the Text to Speech service.");
+// textToSpeechAndPlay("Hello, this is a test of the Text to Speech service.");
+
+module.exports = textToSpeechAndPlay; // export default textToSpeechAndPlay;
